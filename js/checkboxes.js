d72071e185f69c77133880fee875e9bde088fa37
Akigawa <sonoskeakigawa@gmail.com>
2019-02-07 13:13:24 -0800

    Formatting on index.

--- js/checkboxes.js
+++ js/checkboxes.js
@@ -4,12 +4,14 @@ $(document).ready(function(){
     $("#work-responses").show();
     $("input:checkbox[name=stepsCompleted]:checked").each(function(){
       var stepsCompleted = $(this).val();
-      $('#work-responses').append(stepsCompleted + "<br>");
+      $('#stepsCompleted').append(stepsCompleted + "<br>");
     });
     $("#fun-responses").show();
+
     $("input:checkbox[name=modelTests]:checked").each(function(){
       var modelTests = $(this).val();
-      $('#fun-responses').append(modelTests + "<br>");
+
+      $('#modelTests').append(modelTests + "<br>");
     });
     $('#avatarcreation').hide();
   });
